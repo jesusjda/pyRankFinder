495229c6b663f7aedffd0245bac9f8ee0b350534
jesusjda <jesusjda@gmail.com>
2017-05-30 17:42:07 +0200

    others

--- src/RankFinder/RankFinder.py
+++ src/RankFinder/RankFinder.py
@@ -1,1 +1,23 @@
 import os
+import sys
+import getopt
+import GeneralParser as Parser
+from LP import *
+
+def Main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"",[])
+    except getopt.GetoptError:
+        print(help())
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(help())
+        elif opt in ("-a", "--algorithm"):
+            print("To be done..")
+    print("BYE")
+
+if __name__ == "__main__":
+    # Main(sys.argv[1:])
+    x = Variable(0)
+    x._dont
--- src/RankFinder/__init__.py
+++ src/RankFinder/__init__.py
@@ -1,5 +1,5 @@
 import sys
 import RankFinder
 sys.path.append('../../lib/pyParser/src/')
-sys.path.append('../../lib/pyLPi/src/')
+sys.path.append('../../lib/pyLPi/src/lp_ppl/')
 
