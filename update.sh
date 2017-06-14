bb8fdcd86cb25843751cb6145ec6e03f93fbd253
jesusjda <jesusjda@gmail.com>
2017-06-14 15:48:06 +0200

    update script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+getGitDependencies(){
+    echo "Checking git $2"
+	cd $1/lib/$2
+	git pull
+	cd $1
+}
+
+installDependencies(){
+    if [ -f $1/requirements.git ]; then
+	while read GIT; do
+	    getGitDependencies $1 $GIT
+	done <$1/requirements.git
+    fi
+}
+git pull
+installDependencies $(dirname $(readlink -f $0))
