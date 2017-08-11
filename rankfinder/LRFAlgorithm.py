2dba9f7c60c6c1ecc99a444c6f3e205d7609131c
Jesus Domenech <jesusjda@gmail.com>
2017-08-11 12:14:31 +0200

    removing old files

--- rankfinder/LRFAlgorithm.py
+++
@@ -1,9 +0,0 @@
-from TerminationAlgorithm import *
-
-from ppl import Linear_Expression
-from ppl import Variable
-from ppl import Constraint_System
-from ppl import Constraint
-from LPi import C_Polyhedron
-
-
