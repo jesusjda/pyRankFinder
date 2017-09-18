54ef54be40e00efac9c3b1b4f47e9ea9ea98272b
Jesus Domenech <jesusjda@gmail.com>
2017-09-18 18:24:51 +0200

    multiple files

--- runOne.py
+++ runOne.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import traceback
 import getopt
 import argparse
 import termination
@@ -19,17 +20,15 @@ def setArgumentParser():
                            help="increase output verbosity", default=0)
     argParser.add_argument("-ver", "--version", required=False,
                            action='store_true', help="Shows the version.")
-    argParser.add_argument("--dotProgram", required=False,
-                           help="Outfile to show the program as dot graph.")
+    argParser.add_argument("--dotDestination", required=False,
+                           help="Folder to save dot graphs.")
     # Algorithm Parameters
     argParser.add_argument("-dt", "--different_template", action='store_true',
                            help="Use different templates on each node")
-    argParser.add_argument("-sccs", "--scc_strategy", required=False,
-                           default=scc_strategies[0], choices=scc_strategies,
-                           help="Strategy based on SCC to go through the CFG.")
+
     # IMPORTANT PARAMETERS
-    argParser.add_argument("-f", "--file", required=True,
-                           help="File to be analysed.")
+    argParser.add_argument("-f", "--files", nargs='+', required=True,
+                           help="Files to be analysed.")
     argParser.add_argument("-a", "--algorithm", choices=algorithms,
                            required=True, help="Algorithm to be apply.")
     return argParser
@@ -43,21 +42,25 @@ def Main(argv):
         print(_name + " version: " + _version)
         return
     prs = GenericParser()
-    try:
-        if args.dotProgram:
-            cfg = prs.parse(args.file, dot=args.dotProgram)
-            os.system("xdot " + args.dotProgram + " &")
-        else:
-            cfg = prs.parse(args.file)
-    except Exception as e:
-        print(e)
-        return
+    files = args.files
+    for f in files:
+        try:
+            if args.dotDestination:
+                dot = os.path.join(args.dotDestination, f + ".dot")
+                cfg = prs.parse(f, dot=dot)
+                os.system("xdot " + args.dotDestination + " &")
+            else:
+                cfg = prs.parse(f)
+        except Exception as e:
+            print(traceback.format_exc())
+            print(e)
+            return
+        config["cfg"] = cfg
+        internal_config = set_config(config)
 
-    config["cfg"] = cfg
-    internal_config = set_config(config)
-
-    result = termination.run(internal_config)
-    print(result)
+        result = termination.run(internal_config)
+        print(f)
+        print(result)
     return
 
 
@@ -67,24 +70,20 @@ def set_config(data):
         config = {
             "algorithm": "lex",
             "different_template": data["different_template"],
-            "scc_strategy": data["scc_strategy"],
             "cfg": data["cfg"],
             "inner_alg": {
                 "algorithm": data["algorithm"],
                 "different_template": data["different_template"],
-                "scc_strategy": data["scc_strategy"]
             }
         }
     elif data["algorithm"] in ["bmslrf", "bmsnlrf"]:
         config = {
             "algorithm": "bms",
             "different_template": data["different_template"],
-            "scc_strategy": data["scc_strategy"],
             "cfg": data["cfg"],
             "inner_alg": {
                 "algorithm": data["algorithm"],
                 "different_template": data["different_template"],
-                "scc_strategy": data["scc_strategy"],
                 "min_depth": 1,
                 "max_depth": 5
             }
@@ -93,7 +92,6 @@ def set_config(data):
         config = {
             "algorithm": data["algorithm"],
             "different_template": data["different_template"],
-            "scc_strategy": data["scc_strategy"],
             "cfg": data["cfg"]
         }
     return config
