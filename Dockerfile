4de244989d943ab5efb05f6e002816ffa51cc66f
Jesus Domenech <jesusjda@gmail.com>
2017-10-04 17:44:59 +0200

    Docker with easyinterface

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,13 @@ FROM php:5.6-apache
 # docker exec -it rankfinder bash
 RUN apt-get -y update \
  && apt-get -y install git emacs
+
+RUN mkdir /opt/tools && chmod -R 755 /opt/tools
 ADD https://api.github.com/repos/jesusjda/pyRankFinder/git/refs/heads/master version.json
-RUN git clone https://github.com/jesusjda/pyRankFinder
-RUN cd pyRankFinder && ./docker.sh -p=2
+RUN git clone https://github.com/jesusjda/pyRankFinder /opt/tools/pyRankFinder
+RUN cd /opt/tools/pyRankFinder && ./docker.sh -p=2 -p=3
+# RUN git clone https://github.com/abstools/absexamples.git /var/www/absexamples \
+#  && chmod -R 755 /var/www/absexamples \
+RUN git clone https://github.com/jesusjda/easyinterface-config.git /opt/tools/easyinterface-config \
+ && cd /opt/tools/easyinterface-config && ./install.sh --ei-home=/var/www/easyinterface --install-ei
+ENV PYRANKFINDER_HOME /opt/tools/pyRankFinder
--- docker.sh
+++ docker.sh
@@ -15,21 +15,27 @@ install_apt(){
 }
 
 install_all(){
-    for app in $@; do
-	install_apt $app
-    done
+    if [ "$UnixPKG" == "true" ]; then
+	for app in $@; do
+	    install_apt $app
+	done
+    fi
 }
 
-install_module(){
+install_dependencies(){
     vers=$1
     python$vers -m pip install z3 'Cython==0.26' virtualenv
     python$vers -m pip install cysignals 
     python$vers -m pip install git+https://github.com/aleaxit/gmpy.git@gmpy2-2.1.0a0#egg=gmpy2
     python$vers -m pip install git+https://github.com/videlec/pplpy.git#egg=pplpy
-    python$vers -m pip install git+https://github.com/jesusjda/pyLPi.git#egg=pyLPi
     python$vers -m pip install pydot pydotplus
     python$vers -m pip install networkx
     python$vers -m pip install arpeggio
+}
+
+install_module(){
+    vers=$1
+    python$vers -m pip install git+https://github.com/jesusjda/pyLPi.git#egg=pyLPi
     python$vers -m pip install git+https://github.com/jesusjda/pyParser.git#egg=pyParser
     python$vers -m pip install git+https://github.com/jesusjda/pyRankFinder.git#egg=pytermination
 }
@@ -39,6 +45,9 @@ basedir=$(dirname "$(readlink -f "$0" )")
 pvers="false"
 P3=false
 P2=false
+LOCAL=false
+UP=false
+UnixPKG=true
 for i in "$@"; do
     case $i in
 	-p=*|--python=*)
@@ -51,6 +60,18 @@ for i in "$@"; do
 	    fi
 	    shift 
 	    ;;
+	-nu|--no-unix-packages)
+	    UnixPKG=false
+	    shift
+	    ;;
+	-l|--local)
+	    LOCAL=true
+	    shift
+	    ;;
+	-up|--update)
+	    UP=true
+	    shift 
+	    ;;
 	*)
 	    >&2 cat  <<EOF 
 ERROR: docker.sh [OPTIONS]
@@ -58,8 +79,20 @@ ERROR: docker.sh [OPTIONS]
 [OPTIONS]
 
     -p=[VERSION] | --python=[VERSION] )
-                   Install only for python version number [VERSION].
-                   It has to be 2 or 3.
+        Install only for python version number [VERSION].
+        It has to be 2 or 3.
+
+    -nu | --no-unix-packages)
+        Prevent to install unix packages like python-dev or libppl-dev.
+        Use this parameter if you have already satisfied the dependencies.
+
+    -l | --local ) 
+        Install local version with local modifications.
+        Otherwise, git repository version will be installed.
+
+    -up | --update ) 
+        Update or Upgrade all the packages.
+
 
 EOF
 	    exit -1
@@ -68,34 +101,43 @@ EOF
     esac
 done
 
-if [ "$P3" == "false" -a "$P2" == "false"]; then
+if [ "$P3" == "false" -a "$P2" == "false" ]; then
     echo "ERROR at least one python use: -p=[VERSION]" >&2
     exit -1
 fi
-# INSTALL ppl
-install_all libgmp-dev libmpfr-dev libmpc-dev
-if exists ppl-config; then
-    mkdir /tmp/ppl
-    cd /tmp/ppl
-    wget http://bugseng.com/products/ppl/download/ftp/releases/1.2/ppl-1.2.tar.gz
-    gunzip ppl-1.2.tar.gz
-    tar xvfp ppl-1.2.tar
-    cd ppl-1.2
-    ./configure --prefix=/usr/local --with-gmp=/opt/local --enable-interfaces="swi_prolog,c++"
-    make
-    make install
-    # chmod a+rwx -R /usr/local/lib/ppl
+
+if [ "$UnixPKG" == "true" ]; then
+    apt-get -y update
+    # INSTALL ppl
+    install_all libgmp-dev libmpfr-dev libmpc-dev
+
+    if exists ppl-config; then
+	mkdir /tmp/ppl
+	cd /tmp/ppl
+	wget http://bugseng.com/products/ppl/download/ftp/releases/1.2/ppl-1.2.tar.gz
+	gunzip ppl-1.2.tar.gz
+	tar xvfp ppl-1.2.tar
+	cd ppl-1.2
+	./configure --prefix=/usr/local --with-gmp=/opt/local --enable-interfaces="swi_prolog,c++"
+	make
+	make install
+	# chmod a+rwx -R /usr/local/lib/ppl
+    fi
+    install_all libppl-dev graphviz
+
 fi
 
-apt-get -y update
-install_all libppl-dev graphviz
+
+
 if [ "$P2" == "true" ]; then
     install_all -y python python-dev python-nose python-pip
+    install_dependencies 2
     install_module 2
 fi
 
 if [ "$P3" == "true" ]; then
     install_all -y python3 python3-dev python3-nose python3-pip
+    install_dependencies 3
     install_module 3
 fi
 
