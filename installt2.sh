067ded2676a8e93471716c973a51d212cb667905
jesusjda <jesusjda@gmail.com>
2017-10-26 05:35:11 +0200

    remove sudo

--- installt2.sh
+++ installt2.sh
@@ -13,8 +13,8 @@ install_all(){
 
 install_t2(){
     # Add mono sources 
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | tee /etc/apt/sources.list.d/mono-official.list
     
     # Install dependencies
     apt-get update
