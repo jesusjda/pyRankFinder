bb9eb39d6313f75295556445d938c2df47b91441
jesusjda <jesusjda@gmail.com>
2017-10-26 04:05:53 +0200

    fixed t2 install script?

--- installt2.sh
+++ installt2.sh
@@ -12,10 +12,22 @@ install_all(){
 }
 
 install_t2(){
-    install_all build-essential python mono-complete mono-xbuild fsharp
+    # Add mono sources 
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+    
+    # Install dependencies
+    apt-get update
+    # install_all build-essential python mono-devel fsharp ca-certificates-mono 
+
     Z3DIR=/opt/tools/z3
     T2DIR=/opt/tools/t2
     NUGET=/opt/tools/nuget.exe
+
+    # Install nuget
+    curl https://dist.nuget.org/win-x86-commandline/v4.1.0/nuget.exe > $NUGET
+
+    git clone https://github.com/mmjb/T2.git $T2DIR
 
     # Build Z3
     mkdir -p $Z3DIR
@@ -28,30 +40,34 @@ install_t2(){
     cd build
     make
     popd
-    
-    # Install nuget
-    curl https://dist.nuget.org/win-x86-commandline/latest/nuget.exe > $NUGET
 
     # Build .NET bindings for z3
-    pushd $Z3DIR"/code/src/api/dotnet/"
-    xbuild Microsoft.Z3.csproj
+    pushd $Z3DIR"/code/"
+    python scripts/mk_make.py
+    cd build
+    make
+    make install
+    
+    # xbuild Microsoft.Z3.csproj
     popd
 
+
+
     # Update z3 and its .NET bindings in the T2 source tree:
-    cp "$Z3DIR/src/api/dotnet/obj/Debug/Microsoft.Z3.*" "$T2DIR/src/"
-    cp "$Z3DIR/build/libz3.*" "$T2DIR/src/"
+    cp $Z3DIR/code/src/api/dotnet/obj/Debug/Microsoft.Z3.* $T2DIR/src/
+    cp "$Z3DIR/code/build/libz3.*" "$T2DIR/src/"
 
     # Get required packages via NuGet (may need to import certificates first):
-    mozroots --import --sync
+    cert-sync /etc/ssl/certs/ca-certificates.crt
     pushd "$T2DIR/src"
     mono $NUGET restore
     chmod +x packages/FsLexYacc.*/build/*exe
     popd
-
+    
     # Build T2, Release configuration:
     pushd "$T2DIR/src" && xbuild /property:Configuration=Release && popd
 
-    pushd "$T2DIR/test" && mono "$T2DIR/src/bin/Release/T2.exe" -tests
+     #pushd "$T2DIR/test" && mono "$T2DIR/src/bin/Release/T2.exe" -tests
 }
 
 install_t2
