5b057512ec1769efa303061e55473dc3ff273556
Jesus Domenech <jesusjda@gmail.com>
2018-10-02 20:48:15 +0200

    CFRefinement tool: can print the result in sveral formats

--- CFRefinement.py
+++ CFRefinement.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import argparse
-from genericparser import GenericParser
+import genericparser
 from partialevaluation import partialevaluate
 from termination import Output_Manager as OM
+from invariants import compute_invariants
 
 _version = "1.0"
 _name = "CFRefinement"
@@ -25,6 +26,11 @@ def setArgumentParser():
                            help="# times to apply pe", default=1)
     argParser.add_argument("--ei-out", required=False, action='store_true',
                            help="Shows the output supporting ei")
+    argParser.add_argument("-i", "--invariants", required=False,
+                           default="none", help="invariants")
+    argParser.add_argument("-of", "--output_formats", required=False, nargs='+',
+                           choices=["fc", "dot", "koat", "pl", "svg"], default=["fc", "dot", "svg"],
+                           help="Formats to print the graphs.")
     # IMPORTANT PARAMETERS
     argParser.add_argument("-f", "--files", nargs='+', required=True,
                            help="File to be analysed.")
@@ -42,9 +48,9 @@ def buildname(directory, name, num):
     if num == 0:
         f = name
     else:
-        f = name + "pe" + str(num)
+        f = name + "_pe" + str(num)
     f = os.path.join(directory, f)
-    return (f+".fc",f+".dot",f+".svg")
+    return f
 
 def toSvg(dotfile, destination):
     from subprocess import check_call
@@ -61,31 +67,43 @@ def launch(config):
         launch_file(config, f)
         OM.show_output()
 
+def printfile(config, name, destname, cfg, invariant_type):
+    if "fc" in config["output_formats"]:
+        cfg.toFc(path=destname+".fc")
+        OM.writefile(0, name+".fc", file2string(destname+".fc"))
+    if "dot" in config["output_formats"]:
+        cfg.toDot(destname+".dot")
+        if "svg" in config["output_formats"]:
+            toSvg(destname+".dot", destname+".svg")
+            OM.printif(0, file2string(destname+".svg"), format="svg")
+    if "koat" in config["output_formats"]:
+        cfg.toKoat(path=destname+".koat",goal_complexity=True, invariant_type=invariant_type)
+        OM.writefile(0, name+".koat", file2string(destname+".koat"))
+
 def launch_file(config, f):
     try:
         name = extractname(f)
         OM.restart(cdest="file:"+str(name))
-        fcfile, dotfile, svgfile = buildname(config["dir"], name, 0) 
-        os.makedirs(os.path.dirname(fcfile), exist_ok=True)
+        destname = buildname(config["dir"], name, 0)
+        print(destname)
+        os.makedirs(os.path.dirname(destname), exist_ok=True)
         cfg = []
         cfg.append(GenericParser().parse(f))
+        cfg[0].build_polyhedrons()
+        invariant_type=config["invariants"]
+        compute_invariants(cfg[0], invariant_type=invariant_type)
         cfg[0].simplify_constraints()
-        cfg[0].toFc(path=fcfile)
-        cfg[0].toDot(dotfile)
-        toSvg(dotfile, svgfile)
-        OM.printif(0, file2string(svgfile), format="svg")
-        OM.writefile(0, name+".fc", file2string(fcfile))
+        printfile(config, name, destname, cfg[0], invariant_type)
         for i in range(1,config["pe_times"]+1):
-            fcfile, dotfile, svgfile = buildname(config["dir"], name, i) 
-            cfg.append(partialevaluate(cfg[i-1]))
+            destname = buildname(config["dir"], name, i)
+            cfg[i-1].build_polyhedrons()
+            cfg.append(partialevaluate(cfg[i-1], invariant_type=invariant_type))
+            compute_invariants(cfg[i], invariant_type=invariant_type)
             cfg[i].simplify_constraints()
-            cfg[i].toFc(path=fcfile)
-            cfg[i].toDot(dotfile)
-            toSvg(dotfile, svgfile)
-            OM.printif(0, file2string(svgfile), format="svg")
-            OM.writefile(0, name+"_pe"+str(i)+".fc", file2string(fcfile))
+            printfile(config, name+"_pe"+str(i), destname, cfg[i], invariant_type)
     except Exception as e:
         OM.printf("Exception  -> "+str(e))
+        raise Exception() from e
 
 if __name__ == "__main__":
     try:
@@ -98,6 +116,7 @@ if __name__ == "__main__":
             exit(0)
         config = vars(args)
         launch(config)
-    except:
+    except Exception as e:
         OM.show_output()
-        
+        raise Exception() from e
+
