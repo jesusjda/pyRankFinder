b717380bfcfd00969d22acb16ecdf1d1a31f3e81
Jesus Domenech <jesusjda@gmail.com>
2018-11-15 21:33:24 +0100

    non-termination: rename algorithm now is called FIXPOINT and works over close walks instead of cycles. Added algorithm based on MphRFs called Monotonic Recurrent Set. Non-termination algorithms now can go without prefix, you can use  to use them

--- termination/algorithm/nonTermination.py
+++ termination/algorithm/nonTermination.py
@@ -6,86 +6,182 @@ from .manager import Algorithm
 from .manager import Manager
 from termination.output import Output_Manager as OM
 from .utils import get_z3_transition_polyhedron
+from .utils import generate_prime_names
+from .utils import generate_names
+
+from termination import farkas
+from termination.result import Result
+from termination.result import TerminationResult
 
 
-class Renaming(Algorithm):
-    ID = "rename"
-    NAME = "rename"
-    DESC = "Cicle non termination via renaming"
+class FixPoint(Algorithm):
+    ID = "fixpoint"
+    NAME = "fixpoint"
+    DESC = "non termination looking for a fixpoint"
 
     def __init__(self, properties={}):
         self.props = properties
 
-    def run(self, cfg, different_template=False, use_z3=None):
-        cycles = [c for c in cfg.simple_cycles()]
-        if not cycles:
-            return []
+    def run(self, cfg, close_walk):
+        try:
+            init_node = cfg.get_info("init_node")
+        except:
+            init_node = cfg.get_edges()[0]["source"]
+
+        OM.printif(1, "\t\t-> with "+self.NAME)
         global_vars = cfg.get_info("global_vars")
         Nvars = int(len(global_vars) / 2)
         vs = global_vars[:Nvars]
         pvs = global_vars[Nvars:]
-        solutions = []
-        s = Solver()
-        
-        cys = "\n\t".join([" -> ".join(c) for c in cycles])
-        OM.printif(2, "Cycles:\n\t" + cys)
-        for cycle in cycles:
-            cys = ' -> '.join(cycle)
-            OM.printif(2, "Analyzing cycle: " + str(cys))
-            sols = []
-            if len(cycle) == 1:
-                trs = cfg.get_edges(source=cycle[0], target=cycle[0])
-                for tr in trs:
-                    s.push()
-                    rename = [(Real(c), Real(d)) for c, d in zip(vs, pvs)]
-                    rename += [(Real(c), Real(d)) for c, d in zip(pvs, vs)]
-                    for c in get_z3_transition_polyhedron(tr, vs + pvs):
-                        s.add(c)
-                        s.add(substitute(c, rename))
-                    if s.check() == sat:
-                        m = s.model()
-                        sols += [(cycle, m)]
-                    else:
-                        sols += [(cycle, "no recursive state found")]
-                    s.pop()
-            else:
-                names = [vs, pvs]
-                ppvs = pvs
-                primetoken = "'"
-                for _ in range(2, len(cycle)):
-                    ppvs = [v + primetoken for v in ppvs]
-                    names.append(ppvs)
-                names.append(vs)
-                nodes = cycle + [cycle[0]]
-                sols = self._rec_rename(s, cfg, vs, pvs, nodes, names)
-            if sols:
-                OM.printif(1, cycle, ":", sols)
-                solutions += [(cycle, sol) for sol in sols]
-        return solutions
 
-    def _rec_rename(self, solver, cfg, vs, pvs, nodes, names):
-        if len(nodes) == 1:
-            if solver.check() == sat:
-                return [solver.model()]
-            else:
-                return None
-        src = nodes[0]
-        trg = nodes[1]
-        trs = cfg.get_edges(source=src, target=trg)
-        solutions = []
-        for tr in trs:
-            solver.push()
-            rename = [(Real(c), Real(d)) for c, d in zip(vs, names[0])]
-            rename += [(Real(c), Real(d)) for c, d in zip(pvs, names[1])]
+        s = Solver()
+        X = vs[:]
+        XP = pvs[:]
+        used_names = pvs
+        if len(close_walk) == 1:
+            for c in get_z3_transition_polyhedron(close_walk[0], vs + pvs):
+                s.add(c)
+        for tr in close_walk[:-1]:
+            used_names += tr["local_vars"]
+            XP = generate_prime_names(vs,used_names)
+            used_names += XP
+            rename = [(Real(c), Real(d)) for c, d in zip(vs, X)]
+            rename += [(Real(c), Real(d)) for c, d in zip(pvs, XP)]
             for c in get_z3_transition_polyhedron(tr, vs + pvs):
-                solver.add(substitute(c, rename))
-            sols = self._rec_rename(solver, cfg, vs, pvs, nodes[1::], names[1::])
-            if sols:
-                solutions += sols
-            solver.pop()
-        return solutions
+                s.add(substitute(c, rename))
+            X = XP
+        rename = [(Real(c), Real(d)) for c, d in zip(vs, XP)]
+        rename += [(Real(c), Real(d)) for c, d in zip(pvs, vs)]
+        for c in get_z3_transition_polyhedron(close_walk[-1], vs + pvs):
+            s.add(substitute(c, rename))
+        response = Result()
+        if s.check() == sat:
+            m = s.model()
+            OM.printif(1, "\tFixPoint Found")
+            response.set_response(status=TerminationResult.NONTERMINATE,
+                                  info="FixPoint Found",
+                                  close_walk=close_walk,
+                                  model=m)
+        else:
+            OM.printif(1, "\tNo fixpoint found.")
+            response.set_response(status=TerminationResult.UNKNOWN,
+                                  info="No fixpoint found.")
+        return response
+
+
+class MonotonicRecurrentSets(Algorithm):
+    ID = "monotonicrecset"
+    NAME = "monotonic recurrent set"
+    DESC = "Based on MphRF"
+
+    def __init__(self, properties={}):
+        self.props = properties
+
+    @classmethod
+    def generate(cls, data):
+        if(len(data) == 0 or
+           data[0] != cls.ID):
+            return None
+
+        properties = {
+            "name": data[0],
+            "max_depth": 5
+        }
+        data = data[1::]
+        if len(data) == 0:
+            return cls(properties)
+        return None
+
+    def run(self, cfg, close_walk):
+        from ppl import Constraint_System
+        from ppl import Variable
+        from ppl import Linear_Expression
+        from lpi import C_Polyhedron
+        from copy import deepcopy
+        OM.printif(1, "\t\t-> with "+self.NAME)
+        global_vars = cfg.get_info("global_vars")
+        Nvars = int(len(global_vars) / 2)
+        vs = global_vars[:Nvars]
+        pvs = global_vars[Nvars:]
+        lvs = []
+        trvs_idx = []
+        countVar = 0
+
+        for tr in close_walk:
+            lvs += tr["local_vars"]
+            trvs_idx.append(countVar)
+            countVar += Nvars
+        dummy_var = generate_names(["local"+str(i) for i in range(countVar)],global_vars+lvs)
+        farkas_constraints = []
+        countVar -= Nvars
+        countVar = 0
+        ppl_cons = []
+        for tr in close_walk:
+            all_vars = dummy_var[:countVar]+vs+pvs+dummy_var[countVar:]+lvs
+            ppl_cons += [c.transform(all_vars, lib="ppl")
+                         for c in tr["constraints"] if c.is_linear()]
+            countVar += Nvars
+        tr_poly_p = None
+        tr_poly = C_Polyhedron(Constraint_System(ppl_cons), dim=len(all_vars))
+        response = Result()
+        depth = 0
+        pvs_idx = len(close_walk)*(Nvars)
+        while depth < self.get_prop("max_depth"):
+            if tr_poly.is_empty():
+                OM.printif(1, "\tEmpty polyhedron.")
+                response.set_response(status=TerminationResult.UNKNOWN,
+                                      close_walk=close_walk,
+                                      info="No Recurrent Set Found. Empty Polyhedron.")
+                return response
+            Mcons = len(tr_poly.get_constraints())
+            f = [Variable(i) for i in range(0, Nvars + 1)]
+            countVar = Nvars + 1
+            lambdas = [Variable(k) for k in range(countVar, countVar + Mcons)]
+            farkas_constraints = farkas.f(tr_poly, lambdas, f, 0)
+            farkas_poly = C_Polyhedron(Constraint_System(farkas_constraints))
+            generators = farkas_poly.get_generators()
+            tr_poly_p = deepcopy(tr_poly)
+            generators = [g for g in generators if g.is_ray()]
+            OM.printif(2, generators)
+            for g in generators:
+                exp = Linear_Expression(0)
+                for i in range(Nvars):
+                    coef = g.coefficient(Variable(i+1))
+                    exp += coef*Variable(i) - coef*Variable(pvs_idx+i)
+
+                tr_poly_p.add_constraint(exp <= 0)
+
+            if tr_poly_p.contains(tr_poly):
+                OM.printif(1, "\tRecurrent Set Found:\n" +
+                           OM.tostr(tr_poly.get_constraints(), vs+dummy_var+pvs+lvs))
+                response.set_response(status=TerminationResult.NONTERMINATE,
+                                      info="Recurrent Set Found:",
+                                      close_walk=close_walk,
+                                      rec_set=tr_poly)
+                return response
+            tr_poly = tr_poly_p
+            depth += 1
+        OM.printif(1, "\tNo Recurrent Set Found.")
+
+        response.set_response(status=TerminationResult.UNKNOWN,
+                              close_walk=close_walk,
+                              info="No Recurrent Set Found. Max iterations.")
+        return response
 
 
 class NonTermination(Manager):
-    ALGORITHMS = [Renaming]
+    ALGORITHMS = [FixPoint, MonotonicRecurrentSets]
     ID = "non"
+
+    @classmethod
+    def get_algorithm(cls, token):
+        if isinstance(token, str):
+            data = token.split("_")
+        else:
+            data = token
+        for opt in cls.ALGORITHMS:
+            alg = opt.generate(data)
+            if alg is None:
+                continue
+            return alg
+        raise ValueError("Not Valid token")
